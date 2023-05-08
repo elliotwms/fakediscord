08892583cd0788b22145f3d1857977982ae83dbf
Elliot Williams <4396779+elliotwms@users.noreply.github.com>
2023-05-08 23:59:38 +0100

    feat: add user builder, storage and auth

---
+++ internal/fakediscord/storage/users.go
@@ -0,0 +1,23 @@
+package storage
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"sync"
+)
+
+var Users sync.Map
+
+func Authenticate(token string) (u *discordgo.User) {
+	Users.Range(func(key, value any) bool {
+		v := value.(*discordgo.User)
+
+		if v.Token == token {
+			u = v
+			return false
+		}
+
+		return true
+	})
+
+	return
+}
---
+++ internal/fakediscord/storage/users_test.go
@@ -0,0 +1,29 @@
+package storage
+
+import (
+	"github.com/elliotwms/fakediscord/internal/fakediscord/builders"
+	"github.com/elliotwms/fakediscord/internal/snowflake"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	_ = snowflake.Configure(0)
+
+	m.Run()
+}
+
+func TestUsers_Authenticate(t *testing.T) {
+	u := builders.
+		NewUser("foo", "bar").
+		WithToken("foo_token").
+		Build()
+
+	Users.Store(u.ID, u)
+
+	require.NotNil(t, Authenticate("foo_token"))
+}
+
+func TestUsers_Authenticate_TokenNotFound(t *testing.T) {
+	require.Nil(t, Authenticate("notfound"))
+}
